b4525037e30272608a12256613748d90c063f5bf
Jennifer Hill <hillj@janelia.hhmi.org>
2025-09-22 17:39:08 -0400

    calculate affinities in 3D

--- src/mhat/segmentation/affinities.py
+++ src/mhat/segmentation/affinities.py
@@ -3,31 +3,35 @@ import numpy as np
 def compute_affinities(seg: np.ndarray, nhood: list):
     nhood = np.array(nhood)
 
-    shape = seg.shape
-    n_edges = nhood.shape[0]
-    dims = nhood.shape[1]
-    affinity = np.zeros((n_edges,) + shape, dtype=np.int32)
+    shape = seg.shape # Z, Y, X
+    n_edges = nhood.shape[0] # 3
+    dims = nhood.shape[1] # 3
+    affinity = np.zeros((n_edges,) + shape, dtype=np.int32) # 3, Z, Y, X
 
     for e in range(n_edges):
         affinity[
             e,
             max(0, -nhood[e, 0]) : min(shape[0], shape[0] - nhood[e, 0]),
             max(0, -nhood[e, 1]) : min(shape[1], shape[1] - nhood[e, 1]),
+            max(0, -nhood[e, 2]) : min(shape[2], shape[2] - nhood[e, 2]),
         ] = (
             (
                 seg[
                     max(0, -nhood[e, 0]) : min(shape[0], shape[0] - nhood[e, 0]),
                     max(0, -nhood[e, 1]) : min(shape[1], shape[1] - nhood[e, 1]),
+                    max(0, -nhood[e, 2]) : min(shape[2], shape[2] - nhood[e, 2]),
                 ]
                 == seg[
                     max(0, nhood[e, 0]) : min(shape[0], shape[0] + nhood[e, 0]),
                     max(0, nhood[e, 1]) : min(shape[1], shape[1] + nhood[e, 1]),
+                    max(0, nhood[e, 2]) : min(shape[2], shape[2] + nhood[e, 2]),
                 ]
             )
             * (
                 seg[
                     max(0, -nhood[e, 0]) : min(shape[0], shape[0] - nhood[e, 0]),
                     max(0, -nhood[e, 1]) : min(shape[1], shape[1] - nhood[e, 1]),
+                    max(0, -nhood[e, 2]) : min(shape[2], shape[2] - nhood[e, 2]),
                 ]
                 > 0
             )
@@ -35,6 +39,7 @@ def compute_affinities(seg: np.ndarray, nhood: list):
                 seg[
                     max(0, nhood[e, 0]) : min(shape[0], shape[0] + nhood[e, 0]),
                     max(0, nhood[e, 1]) : min(shape[1], shape[1] + nhood[e, 1]),
+                    max(0, nhood[e, 2]) : min(shape[2], shape[2] + nhood[e, 2]),
                 ]
                 > 0
             )

eb4803e6be27a30ce459768de5ff342a0a567990
Jennifer Hill <hillj@janelia.hhmi.org>
2025-10-29 15:04:37 -0400

    Add visualization of rocks and rock segmentation

--- scripts/03_tracking/visualize_results.py
+++ scripts/03_tracking/visualize_results.py
@@ -1,66 +1,77 @@
 from pathlib import Path
 import napari
 import zarr
+import networkx as nx
 import pandas as pd
+import numpy as np
+from typing import Any
 
+from motile_toolbox.candidate_graph import NodeAttr
 from motile_tracker.import_export.load_tracks import tracks_from_df
 from motile_tracker.application_menus import MainApp
 from motile_tracker.data_views.views_coordinator.tracks_viewer import TracksViewer
 
 
-def main(raw_zarr_path, track_data_path, exp_name):
 
-    pred_tracks_path = track_data_path / exp_name / 'pred_tracks.csv'
-    pred_seg_path = track_data_path / exp_name / 'pred_seg.zarr'
+def main(dataset_name, exp_number, exp_name):
 
-    raw = zarr.open(raw_zarr_path, mode='r')
-    raw = raw[:, 0, ...]
-    print(f"Raw shape: {raw.shape}, dtype: {raw.dtype}")
+    data_path = Path('Y:\\jennifer\\mhat\\data')
+    experiment_path = Path('Y:\\jennifer\\mhat\\experiments')
 
-    # load the segmentation data if available
-    try:
-        pred_seg = zarr.open(pred_seg_path, mode='r')
-        pred_seg = pred_seg[:]
-        print(f"Segmentation shape: {pred_seg.shape}, dtype: {pred_seg.dtype}")
-    except:
-        pred_seg = None
-        print(f"Segmentation not found at {pred_seg_path}, loading only raw image and tracks.")
+    raw_cells_zarr_path = data_path / dataset_name / f"{exp_number:02d}_cells.zarr"
+    raw_rocks_zarr_path = data_path / dataset_name / f"{exp_number:02d}_rocks.zarr"
+    frag_zarr_path = experiment_path / 'segmentation' / dataset_name / f"{exp_number:02d}_cells" / 'data.zarr'
+    rocks_seg_zarr_path = experiment_path / 'segmentation' / dataset_name / f"{exp_number:02d}_rocks" / 'data.zarr'
+    track_seg_zarr_path = experiment_path / 'tracking' / dataset_name / f"{exp_number:02d}_cells" / exp_name / 'pred_seg.zarr'
 
-    # load the tracks data
-    csvfile = pred_tracks_path
-    selected_columns = {
-        "time": "time",
-        "y": "y",
-        "x": "x",
-        "id": "id",
-        "parent_id": "parent_id",
-        "seg_id": "id",
-    }
+    # Check if all paths exist
+    for path in [raw_cells_zarr_path, raw_rocks_zarr_path, frag_zarr_path, rocks_seg_zarr_path, track_seg_zarr_path]:
+        if not path.exists():
+            print(f"Warning: Path {path} does not exist.")
+            path = None
 
-    df = pd.read_csv(csvfile)
-
-    for feature, column in selected_columns.items():
-        df[feature] = df[column]
-
-    tracks = tracks_from_df(
-        df=df,
-        scale=[1, 1, 1, 1],
-    )
-
+    if all(path is None for path in [raw_cells_zarr_path, raw_rocks_zarr_path, frag_zarr_path, rocks_seg_zarr_path, track_seg_zarr_path]):
+        raise FileNotFoundError("None of the required data paths exist. Exiting.")
+    
     viewer = napari.Viewer()
-    viewer.add_image(raw, name='raw')
-    if pred_seg is not None:
-        viewer.add_labels(pred_seg, name='pred_seg', opacity=0.5)
+
+    if raw_cells_zarr_path is not None:
+        raw_cells = zarr.open(raw_cells_zarr_path, mode='r')
+        raw_cells = raw_cells[:, 0, ...]
+        print(f"Raw shape: {raw_cells.shape}, dtype: {raw_cells.dtype}")
+        viewer.add_image(raw_cells, name='raw', colormap='purple', blending='additive')
+
+    if raw_rocks_zarr_path is not None:
+        raw_rocks = zarr.open(raw_rocks_zarr_path, mode='r')
+        raw_rocks = raw_rocks[:, 0, ...]
+        print(f"Rocks shape: {raw_rocks.shape}, dtype: {raw_rocks.dtype}")
+        viewer.add_image(raw_rocks, name='rocks', colormap='gray', blending='additive')
+
+    if frag_zarr_path is not None:
+        fragments = zarr.open(frag_zarr_path, mode='r')
+        fragments = fragments['fragments'][:, ...]
+        print(f"Fragments shape: {fragments.shape}, dtype: {fragments.dtype}")
+        viewer.add_labels(fragments, name='fragments')
+
+    if rocks_seg_zarr_path is not None:
+        rocks_seg = zarr.open(rocks_seg_zarr_path, mode='r')
+        rocks_seg = rocks_seg['segmentations'][:, ...]
+        print(f"Rocks segmentation shape: {rocks_seg.shape}, dtype: {rocks_seg.dtype}")
+        viewer.add_labels(rocks_seg, name='rocks_seg', opacity=0.5)
+
+    if track_seg_zarr_path is not None:
+        track_seg = zarr.open(track_seg_zarr_path, mode='r')
+        track_seg = track_seg[:]
+        print(f"Tracked segmentation shape: {track_seg.shape}, dtype: {track_seg.dtype}")
+        viewer.add_labels(track_seg, name='track_seg', opacity=0.5)
+
     widget = MainApp(viewer)
     viewer.window.add_dock_widget(widget)
-    TracksViewer.get_instance(viewer).tracks_list.add_tracks(tracks, "example")
 
     napari.run()
 
 if __name__ == '__main__':
-    # raw_zarr_path = Path('/groups/sgro/sgrolab/jennifer/mhat/data/mixin63/02_test_data.zarr')
-    # track_data_path = Path('/groups/sgro/sgrolab/jennifer/mhat/experiments/tracking/mixin63/03_test_data')
-    raw_zarr_path = Path('Y:\\jennifer\\mhat\\data\\mixin63\\01_test_data.zarr')
-    track_data_path = Path('Y:\\jennifer\\mhat\\experiments\\tracking\\mixin63\\01_test_data')
-    exp_name = "2025-10-16_11-04-32"
-    main(raw_zarr_path, track_data_path, exp_name)
+    main(dataset_name="nc281-spiAmSG", 
+         exp_number=1, 
+         exp_name="2025-10-28_17-42-55",
+         )

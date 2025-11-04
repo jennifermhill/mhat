2da7a26da291f2fb0ffc604a8e19c2271fb724e3
Jennifer Hill <hillj@janelia.hhmi.org>
2025-11-04 13:51:52 -0500

    Add mac filepaths

--- scripts/02_segmentation/visualize_results.py
+++ scripts/02_segmentation/visualize_results.py
@@ -29,6 +29,8 @@ def main(raw_zarr_path, seg_zarr_path):
     napari.run()
 
 if __name__ == '__main__':
-    raw_zarr_path = '/groups/sgro/sgrolab/jennifer/mhat/data/mixin63/02_test_data.zarr'
-    seg_zarr_path = '/groups/sgro/sgrolab/jennifer/mhat/experiments/segmentation/mixin63/03_test_data/data.zarr'
+    raw_zarr_path = '/groups/sgro/sgrolab/jennifer/mhat/data/nc281-spiAmSG_agg/02_cells.zarr'
+    seg_zarr_path = '/groups/sgro/sgrolab/jennifer/mhat/experiments/segmentation/nc281-spiAmSG_agg/02_cells/data.zarr'
+    # raw_zarr_path = '/Volumes/sgrolab/jennifer/mhat/data/nc281-spiAmSG/01_cells.zarr'
+    # seg_zarr_path = '/Volumes/sgrolab/jennifer/mhat/experiments/segmentation/nc281-spiAmSG/01_cells/data.zarr'
     main(raw_zarr_path, seg_zarr_path)

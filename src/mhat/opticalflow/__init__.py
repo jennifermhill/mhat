e1b5867cf17e12f3938bddc198d9e589f2be7cd1
Jennifer Hill <hillj@janelia.hhmi.org>
2026-01-16 17:41:03 -0500

    Add farneback to init

--- src/mhat/opticalflow/__init__.py
+++ src/mhat/opticalflow/__init__.py
@@ -1,5 +1,11 @@
+from .farneback import compute_farneback_flow_2d, compute_farneback_flow_3d
 from .utils import enhance_contrast_AHE
 from .visualization import generate_flow_frames
 
 __version__ = "0.1.0"
-__all__ = ["enhance_contrast_AHE", "generate_flow_frames"]
+__all__ = [
+    "compute_farneback_flow_2d", 
+    "compute_farneback_flow_3d", 
+    "enhance_contrast_AHE", 
+    "generate_flow_frames"
+    ]

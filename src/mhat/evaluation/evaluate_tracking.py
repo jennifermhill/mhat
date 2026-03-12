d006c909890f502ebfa9ca677dee505601c90599
Jennifer Hill <hillj@janelia.hhmi.org>
2026-03-12 15:53:30 -0400

    Add all traccuracy metrics as options to be specified in eval_config

--- src/mhat/evaluation/evaluate_tracking.py
+++ src/mhat/evaluation/evaluate_tracking.py
@@ -1,19 +1,28 @@
 from traccuracy import TrackingGraph, run_metrics
 from traccuracy.matchers import PointMatcher
-from traccuracy.metrics import BasicMetrics, TrackOverlapMetrics
+import traccuracy.metrics as metrics
 
+metrics_dict = {
+    "basic": metrics.BasicMetrics,
+    "cca": metrics.CellCycleAccuracy,
+    "complete_tracks": metrics.CompleteTracks,
+    "aogm": metrics.AOGMMetrics,
+    "ctc": metrics.CTCMetrics,
+    "division": metrics.DivisionMetrics,
+    "track_overlap": metrics.TrackOverlapMetrics,
+}
 
 def evaluate_tracking(
-    gt_graph, pred_graph, gt_segmentation=None, pred_segmentation=None, match_threshold=5
+    config, gt_graph, pred_graph, gt_segmentation=None, pred_segmentation=None
 ):
     """Calculate metrics for linked tracks by comparing to ground truth.
 
     Args:
+        config (dict): Evaluation configuration dictionary.
         gt_graph (networkx.DiGraph): Ground truth graph.
         gt_segmentation (np.ndarray): Ground truth detections.
         pred_graph (networkx.DiGraph): Predicted graph.
         pred_segmentation (np.ndarray): Predicted dense segmentation.
-        match_threshold (float): Distance threshold for matching tracks.
 
     Returns:
         results (dict): Dictionary of metric results.
@@ -35,11 +44,19 @@ def evaluate_tracking(
         segmentation=pred_segmentation,
     )
 
+    match_threshold = config.get("match_threshold", 5.0)
+    metrics = config.get("metrics", ["basic", "track_overlap"])
+
+    # Check that the specified metrics are valid
+    for metric in metrics:
+        if metric not in metrics_dict:
+            raise ValueError(f"Invalid metric specified: {metric}\nValid metrics are: {list(metrics_dict.keys())}")
+
     results, matched = run_metrics(
         gt_graph,
         pred_graph,
         matcher=PointMatcher(threshold=match_threshold),
-        metrics=[BasicMetrics(), TrackOverlapMetrics()],
+        metrics=[metrics_dict[m]() for m in metrics],
     )
 
     return results

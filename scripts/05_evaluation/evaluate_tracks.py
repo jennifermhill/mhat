38ae01c13178be109fd7125b8c377f6c889e18eb
Jennifer Hill <hillj@janelia.hhmi.org>
2026-03-12 15:48:59 -0400

    Convert CTC ground truth to geff for evaluation

--- scripts/05_evaluation/evaluate_tracks.py
+++ scripts/05_evaluation/evaluate_tracks.py
@@ -7,6 +7,7 @@ import geff
 import zarr
 
 from mhat.evaluation.evaluate_tracking import evaluate_tracking
+from mhat.evaluation.from_ctc_to_geff import from_ctc_to_geff
 
 
 def run_evaluation(config, gt_data_dir, pred_data_dir):
@@ -20,14 +21,27 @@ def run_evaluation(config, gt_data_dir, pred_data_dir):
         print(f"Warning: Predicted segmentation zarr not found at {pred_segmentation_path}")
         pred_segmentation = None
 
+        # Check for CTC metrics
+    if "ctc" in config.get("metrics", []):
+        # Check if gt has already been converted to geff format
+        if not gt_tracks_path.is_dir():
+            # If not convert to geff format
+            print(f"Converting GT tracks to geff format at {gt_tracks_path}")
+            from_ctc_to_geff(
+                ctc_path=gt_data_dir / "01_GT" / "TRA",
+                geff_path=gt_tracks_path,
+                segmentation_store=gt_data_dir / "correct_seg.zarr",
+            )
+
     (gt_graph, gt_metadata) = geff.read(gt_tracks_path)
     (pred_graph, pred_metadata) = geff.read(pred_tracks_path)
 
     results = evaluate_tracking(
-        gt_graph, pred_graph, 
+        config,
+        gt_graph, 
+        pred_graph, 
         gt_segmentation=None,
         pred_segmentation=None,
-        match_threshold=config["match_threshold"],
     )
 
     track_metrics = {}

c89f9c1254f8f743d5bcfbfdfce948971daea9ba
Jennifer Hill <hillj@janelia.hhmi.org>
2026-07-01 13:48:45 -0400

    Fix config states

--- scripts/02_segmentation/visualize_results.py
+++ scripts/02_segmentation/visualize_results.py
@@ -45,8 +45,8 @@ def main(raw_zarr_path, seg_zarr_path, compute=False):
 if __name__ == '__main__':
     # raw_zarr_path = '/groups/sgro/sgrolab/jennifer/mhat/data/NC281-Fl2mSiH2B/02_cells.zarr'
     # seg_zarr_path = '/groups/sgro/sgrolab/jennifer/mhat/experiments/segmentation/NC281-Fl2mSiH2B/03_test_data/data.zarr'
-    raw_zarr_path = "Y:\\jennifer\\mhat\\data\\NC281-Fl2mSiH2B\\03_nuclei.zarr"
-    seg_zarr_path = 'Y:\\jennifer\\mhat\\experiments\\segmentation\\NC281-Fl2mSiH2B\\03_nuclei\\2026-04-22_10-04-15\\data.zarr'
+    raw_zarr_path = "Y:\\jennifer\\mhat\\data\\NC281-sparse-label\\01_nuclei_denoised_train.zarr"
+    seg_zarr_path = 'Y:\\jennifer\\mhat\\experiments\\segmentation\\NC281-sparse-label\\01_nuclei_denoised_train\\2026-07-01_12-00-16\\data.zarr'
     # raw_zarr_path = '/Volumes/sgrolab/jennifer/mhat/data/Fusion_vol2/00_nuclei_denoised.zarr'
     # seg_zarr_path = '/Volumes/sgrolab/jennifer/mhat/experiments/segmentation/Fusion_vol2/00_nuclei_denoised/2026-03-03_18-13-58/data.zarr'
     main(raw_zarr_path, seg_zarr_path, compute=False)
--- scripts/03_opticalflow/visualize_results.py
+++ scripts/03_opticalflow/visualize_results.py
@@ -105,6 +105,6 @@ def main(config, compute: bool = False):
 
 
 if __name__ == "__main__":
-    path_to_config = "Y:\\jennifer\\mhat\\experiments\\opticalflow\\Fluo-C3DL-MDA231\\01_cells\\opticalflow_2d\\2026-02-17_15-55-00\\config.toml"
+    path_to_config = "Y:\\jennifer\\mhat\\experiments\\opticalflow\\NC281-sparse-label\\01_nuclei_denoised_train\\opticalflow_2d\\2026-07-01_13-20-56\\config.toml"
     config = toml.load(path_to_config)
     main(config, compute=True)

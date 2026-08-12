fe386b9f874ac938cea30e0b1d5083b52a5abeda
Jennifer Hill <hillj@janelia.hhmi.org>
2026-08-12 16:39:05 -0400

    Remove redundant report_graph_statistics call

--- scripts/04_tracking/run_tracking.py
+++ scripts/04_tracking/run_tracking.py
@@ -242,8 +242,6 @@ def run_tracking(config, raw_dir: Path, seg_dir: Path, flow_dirs: dict, output_d
     utils.add_area_diff_attr(track_graph)
     utils.add_intensity_diff_attr(track_graph)
 
-    report_graph_statistics(config, track_graph)
-
     # Save candidate edge list for analysis
     cand_edges_path = output_dir / "candidate_edges.npy"
     cand_edge_list = np.array([(e[0], e[1]) for e in track_graph.edges], dtype=np.int64)
